cf4e1c7c6df234c96e6405329fcf881581c76e2f
Rushil-2311 <rushilpatellinux2gmail.com>
2022-02-02 10:42:16 +0530

    routing-with-dynamic-path

--- pages/post/index.js
+++ pages/post/index.js
@@ -21,6 +21,21 @@ const Post = () => {
             <a>Second comment</a>
           </Link>
         </li>
+        <li>
+          <Link href="/post/[id]/[comment]" as={`/post/${id}/second-comment`}>
+            <a>Second comment</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/post/[id]/[comment]" as={`/post/${id}/second-comment`}>
+            <a>Second comment</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/post/[id]/[comment]" as={`/post/${id}/second-comment`}>
+            <a>Second comment</a>
+          </Link>
+        </li>
       </ul>
     </>
   );
